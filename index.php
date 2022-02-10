5ac1d8bb8a9e261cdad7182e28a4c8a94dd18cb7
Wendy Salinas-Reyes <wendy3salinas@gmail.com>
2022-02-10 09:17:52 -0800

    adds text about pet choices

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 </head>
 <body>
 <h2>AJAX Pet Adoption Agency</h2>
-<p>Below is a starter page for the AJAX Pet Adoption Agency assignment.</p>
+<p>Make some changes and reveal your pet!</p>
 <div id="output">
 <form id="myForm" action="" method="get">
 
